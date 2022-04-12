2251d2592355229883f088d6d202beb2a6ab19af
Your NameTitín <titin.colomer.romero@gmail.com>
2022-04-12 10:45:14 +0200

    ruta ahuth no la encuentra

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,39 +1,39 @@
 /*
     Rutas de usuarios / auth
     host + /api/auth
-
 */
-
-
 const { Router } = require('express');
 const { check } = require('express-validator');
-const router = Router();
-
-const{crearUsuario, loginUsuario, revalidarToken} = require('../controlers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controlers/auth');
 const { validarJWT } = require('../middlewares/validar-jwt');
+
+
+const router = Router();
 
 
 router.post(
     '/new', 
-    [ //midlewares
+    [ // middlewares
         check('name', 'El nombre es obligatorio').not().isEmpty(),
-        check('email', 'El Correo electrónico es obligatorio').isEmail(),
-        check('password', 'La contraseña debe tener 6 carácteres').isLength({min:6}),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
         validarCampos
     ],
-    crearUsuario
+    crearUsuario 
 );
 
-router.post('/',
-    [ //midlewares
-    check('email', 'El Correo electrónico es obligatorio').isEmail(),
-    check('password', 'La contraseña debe tener 6 carácteres').isLength({min:6}),
-    validarCampos
+router.post(
+    '/',
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
     ],
-    loginUsuario
+    loginUsuario 
 );
 
-router.get('/renew', validarJWT, revalidarToken );
+
+router.get('/renew', validarJWT ,revalidarToken );
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const express = require('express');
-var cors = require('cors');
-const { dbConnection } = require('./database/config');
 require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
 
-//Crear servidor de Express
+// Crear el servidor de express
 const app = express();
 
-//BBDD
+// Base de datos
 dbConnection();
 
-//CORS
-app.use(cors());
+// CORS
+app.use(cors())
 
-// Lectura y Parseo del body
+// Directorio Público
+app.use( express.static('public') );
+
+// Lectura y parseo del body
 app.use( express.json() );
 
-//Directorio público
-app.use(express.static('public'));
+// Rutas
+app.use('/api/auth', require('./routes/auth') );
+app.use('/api/events', require('./routes/events') );
 
-//Rutas
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/events', require('./routes/events'));
 
-//Escuchar peticiones
-app.listen(process.env.PORT, ()=> {
-    console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`);
-})
+
+
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
+
+
+
+
+
+
